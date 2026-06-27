a98c2c4d826135f8ebd07f35c32f91062faed218
rickdev8 <rique88858@gmail.com>
2026-06-27 05:14:14 -0300

    resolve any type

--- src/utils/define-rank.ts
+++ src/utils/define-rank.ts
@@ -1,6 +1,6 @@
 function DefineRank(users: any) {
     const ranking = users
-        .sort((a, b) => b.score - a.score)
+        .sort((a: any, b: any) => b.score - a.score)
         .map((user: any, index: number) => ({
             ...user,
             rank: index + 1,
